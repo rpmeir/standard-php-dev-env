855d104570509a2704e3225a3a406d4b382d6b93
Rodrigo Pires Meira <rpmeir@gmail.com>
2022-03-05 09:55:09 -0300

    change url to fetch php extension installer

--- .docker/.shared/scripts/install_php_extensions.sh
+++ .docker/.shared/scripts/install_php_extensions.sh
@@ -5,7 +5,8 @@ apt-get update -yqq && apt-get -f install -yyq wget
 
 # download helper script
 # @see https://github.com/mlocati/docker-php-extension-installer/
-wget -q -O /usr/local/bin/install-php-extensions https://raw.githubusercontent.com/mlocati/docker-php-extension-installer/master/install-php-extensions \
+wget -q -O /usr/local/bin/install-php-extensions \
+    https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions \
     || (echo "Failed while downloading php extension installer!"; exit 1)
 
 # install extensions
